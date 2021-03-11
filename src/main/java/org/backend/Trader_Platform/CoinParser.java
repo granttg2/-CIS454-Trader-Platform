fa480a115a0fa88d25960424d784b62cd8593bfe
Matt <Matt@LAPTOP-4S0B42SD.fios-router.home>
2021-03-11 16:32:10 -0500

    Parser for CoinBase

---
+++ src/main/java/org/backend/Trader_Platform/CoinParser.java
@@ -0,0 +1,36 @@
+package org.backend.Trader_Platform;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+import org.json.*;
+import org.jfx.Trader_Platform.App;
+
+public class CoinParser {
+
+	public static void parseCoinEODTotals(Stock stock){
+		String start = stock.getStartDate().toString();
+		String end = stock.getEndDate().toString();
+		String jsonString;
+		LocalDate lDate = stock.getEndDate(); 
+		
+		try {
+			jsonString = CoinCall.getCoinHistory(stock.getTicker(), start, end);
+			JSONArray completeA = new JSONArray(jsonString);
+
+			for(int i = completeA.length() - 1; i >= 0; i--) {
+				JSONArray object = completeA.getJSONArray(i);
+				stock.addNewEOD(lDate.toString(),object.getDouble(4));
+				lDate.minusDays(1);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+
+}
+
